4d8f96fddddeefcebd3d7c1b96440e42105883d2
Seung ho Jung <jungseungho@gmail.com>
2017-12-06 15:05:46 -0500

    v0.3
    Opening Title audioFX, trying to make limbs to act as thrusts (AddRelativeForce to parent?)

---
+++ Magic Girl Mecha - Beauty Pretty Society/Assets/Scripts/Thrust.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour {
+
+    public float thrustForce = 50f;
+    public GameObject center;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("z") == true)
+        {
+            this.GetComponent<Rigidbody>().AddRelativeForce(0, 0, thrustForce);
+        }
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.tag == "center")
+    //    {
+    //        transform.parent.gameObject.rigidbody.AddForceAtPosition(thrustForce, transform.position);
+    //    }
+    //}
+}
---
+++ Magic Girl Mecha - Beauty Pretty Society/Assets/Scripts/loadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour {
+
+    public string levelToLoad;
+    public Color loadToColor = Color.black;
+
+    private void Update()
+    {
+        if (Input.anyKey)
+            Initiate.Fade(levelToLoad, loadToColor, 3.0f);
+    }
+}
