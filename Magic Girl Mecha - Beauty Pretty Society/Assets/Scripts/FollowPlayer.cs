9c362583d97fd13cef0dfc8aac0508cb6cee1bae
Seung ho Jung <jungseungho@gmail.com>
2017-12-06 04:22:02 -0500

    v0.2
    Basic game scene

---
+++ Magic Girl Mecha - Beauty Pretty Society/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+    [Range(0f, 50f)]
+    public float smoothSpeed = 0.125f;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = player.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPos;
+        //transform.LookAt(player);
+    }
+}
